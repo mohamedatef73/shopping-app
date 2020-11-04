4f83ad537840dea59d95c2477b485d2b38c188fc
mohamed atef <59102121+mohamedatef73@users.noreply.github.com>
2020-11-04 19:55:17 +0200

    create react app and install react router dom , bootstrap and deleating the un necessary files

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Home Page </h1>
+    </div>
+  );
+}
+
+export default App;
